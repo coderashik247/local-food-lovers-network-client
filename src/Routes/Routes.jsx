14be2f14640a9096cc4a5814de4bba1600ad3c87
Ashik <ashikurashik20999@gmail.com>
2025-11-10 00:56:17 +0600

    basic setup add

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+import MainLayouts from "../layouts/MainLayouts";
+
+
+export const router = createBrowserRouter([
+    {
+        path:"/",
+        Component: MainLayouts
+    }
+])
---
+++ src/layouts/MainLayouts.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainLayouts = () => {
+    return (
+        <div>
+            <h2>Main Layouts</h2>
+        </div>
+    );
+};
+
+export default MainLayouts;
